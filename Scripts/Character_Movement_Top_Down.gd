6355a6809e7bdb40402f7f2253d0c3aed96a7ed3
fliro76 <fliro01@hotmail.com>
2025-07-13 01:05:31 -0400

    Mouvement, basic level 1 scene

--- Scripts/Character_Movement_Top_Down.gd
+++ Scripts/Character_Movement_Top_Down.gd
@@ -2,21 +2,22 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = 300
 
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+func _physics_process(_delta: float) -> void:
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+
+	var ydirection := Input.get_axis("Move_Up", "Move_Down")
+	if ydirection:
+		velocity.y = ydirection * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("Move_Left", "Move_Right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
