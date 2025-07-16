7bdc6483bca88569c57028034bf4114e0f62df71
Kwino0 <boivinoo@outlook.fr>
2025-07-16 19:26:31 -0400

    Added a Game Manager.
    From QA eyes nothing should have changed.
    
    Things that might be affected:
    - Main Menu/Pause menu
    - Changing levels
    
    Changed the Main node to GameController (From main menu) but the game controller loads the main menu anyway so no change should be observe

--- Scripts/StartGame.gd
+++ Scripts/StartGame.gd
@@ -3,4 +3,6 @@ extends Button
 
 #Load the Level_0 scene
 func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Levels/Level/Houses/level_1.tscn")
+	Global.game_controller.change_world2D_scene("res://Scenes/Levels/Level/Houses/level_1.tscn")
+	Global.game_controller.change_gameUI_scene("res://Scenes/Miscelanious/UI/pause_manager.tscn")
+	#get_tree().change_scene_to_file("res://Scenes/Levels/Level/Houses/level_1.tscn")
